1550d4bbb3b41f4ed5cf3892352c5b2bed0dd118
NalakaDH <152781268+NalakaDH@users.noreply.github.com>
2023-12-08 16:50:18 +0530

    Changed font-color button

--- app.js
+++ app.js
@@ -7,13 +7,12 @@ function bold()   {
  function italic()   {
     document.execCommand('italic', true , null) 
  } 
- $('.font-color').on('click', function() {
-    var selection = window.getSelection()
-    var highlightedText = selection.toString()
-    var span = "<span style='color: red'>" + highlightedText + "</span>"
-    var text = document.getElementById('editor').innerHTML
-    document.getElementById('editor').innerHTML = text.replace(highlightedText, span)
-})
+ $('.dropdown-menu .color-option').on('mousedown', function () {
+    var fontColor = this.dataset.color;
+    document.execCommand('foreColor', false, fontColor);
+    document.getElementById('editor').focus();
+    return false; // Prevents the button from losing focus
+ });
 $('.left').on('click',function(){
     document.getElementById('editor').style.textAlign = 'left '
  }) 
